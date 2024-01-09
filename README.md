b187d03c0c6ce2b7af841c3022176c489394406d
Imran Hashmi <112310959+Imran6500@users.noreply.github.com>
2024-01-10 00:44:21 +0530

    contact list

---
+++ README.md
@@ -0,0 +1,11 @@
+# contact_details
+
+A new Flutter project to show the contact list
+
+steps -
+
+1. add package
+2. add code in AndroidManifest.xml for permission
+3. write functions to show the contact list.
+
+that's it....
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:fast_contacts/fast_contacts.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "All Contact",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SizedBox(
+        height: double.infinity,
+        child: FutureBuilder(
+          future: getContacts(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(
+                child: SizedBox(height: 50, child: CircularProgressIndicator()),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  Contact contact = snapshot.data[index];
+                  return Column(children: [
+                    ListTile(
+                      leading: const CircleAvatar(
+                        radius: 20,
+                        child: Icon(Icons.person),
+                      ),
+                      title: Text(contact.displayName),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(contact.phones.toString())
+
+                          // Text(contact.emails.toString()),
+                        ],
+                      ),
+                    ),
+                    const Divider()
+                  ]);
+                });
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<List<Contact>> getContacts() async {
+    bool isGranted = await Permission.contacts.status.isGranted;
+    if (!isGranted) {
+      isGranted = await Permission.contacts.request().isGranted;
+    }
+    if (isGranted) {
+      return await FastContacts.getAllContacts();
+    }
+    return [];
+  }
+}
